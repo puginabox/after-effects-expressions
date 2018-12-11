f3fb14a5e9348b1cd303018b7b55059e2ee41744
Griffin Byron <puginabox@gmail.com>
2018-12-11 12:31:18 -0500

    Create text-rect_self_resize&color.js

---
+++ text-rect_self_resize&color.js
@@ -0,0 +1,8 @@
+
+// Auto-Resizable Colorize Rectangle
+
+title = thisComp.layer("Rectangle").sourceRectAtTime();
+padding = effect("Padding")("Slider");
+[title.width + padding, title.height + padding]
+
+// change named Rectangle to your text layer name 
