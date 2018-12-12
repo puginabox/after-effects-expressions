343dc7bf6ffab7864b46446a305554d1f4e85632
Griffin Byron <puginabox@gmail.com>
2018-12-12 17:54:02 -0500

    Update text-rect_self_resize&color.js

--- text-rect_self_resize&color.js
+++ text-rect_self_resize&color.js
@@ -5,4 +5,7 @@ title = thisComp.layer("Rectangle").sourceRectAtTime();
 padding = effect("Padding")("Slider");
 [title.width + padding, title.height + padding]
 
-// change named Rectangle to your text layer name 
+// change named Rectangle to your text layer name (IE: text-to-resize)
+
+
+title = thisComp.layer("Text to Change").sourceRectAtTime();padding = effect("Background Padding")("Slider");[title.width + padding, title.height + padding]
